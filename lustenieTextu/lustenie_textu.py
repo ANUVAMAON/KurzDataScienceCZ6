00dfecce9270b7eed5b6bef23c2a753bde161881
ANUVAMAON <martin.ondrejka@anuva.eu>
2023-05-20 18:58:02 +0200

    first commit

---
+++ lustenieTextu/lustenie_textu.py
@@ -0,0 +1,31 @@
+import os
+import pickle
+import json
+
+files = os.listdir()
+
+text = ""
+
+for file in files:
+    if file.endswith("txt"):
+        f = open(file, "r", encoding='cp1250')
+        text += f.read() + " "
+        f.close()
+
+for file in files:
+    if file.endswith("obj"):
+        objects = []
+        with open(file, 'rb') as openfile:
+            objects.append(pickle.load(openfile))
+
+        text += objects[0] + " "
+        openfile.close
+
+for file in files:
+    if file.endswith("json"):
+        with open(file, 'rb') as openfile:
+            text_in_file = json.load(openfile)
+            text += text_in_file + " "
+            openfile.close
+
+print(text)
